01ff2fb2662d767d42d1677732f32e289f7edf8b
ShinzDev <shinaayomibamgboye@gmail.com>
2026-05-14 16:20:59 +0100

    update index.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/', // <-- CRITICAL FIX: Make sure this is just a slash
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
